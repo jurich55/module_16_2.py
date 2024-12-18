0f7fb26e2c6a80f46c0e994f241d843512ac7066
Vladimir <sv_urich@mail.ru>
2024-12-18 10:13:12 +0400

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -11,33 +11,47 @@ async def in_put():
 
 @app.get("/user/admin")
 async def get_admin(admin: str = 'Fedor'):
-    return {"message": f"Имя: {admin}, Вы вошли как администратор"}
+    return {"message": f"Имя: {admin},"
+                       f" Вы вошли как администратор"}
 
 @app.get('/user/{user_id}')
 async def get_user(
-    user_id: Annotated
-    [int,Path(
+    user_id:Annotated[
+    int,Path(
         ge=1,
         le=100,
-        description="Enter your id")
+        description='Введите свой ID'
+                    '(положительное целое число от 1 до 100)'
+      )
     ]
 ):
     return {"message": f'Hello, {user_id}'}
 
 @app.put('/user/{username}/{age}')
 async def put_user(
-    username: Annotated
+    username:Annotated
     [str,Path(
         min_length=5,
         max_length=20,
-        description='Enter username')
-    ], age:int = Path(
+        description='Имя пользователя'
+                    '(от 5 до 20 символов)'
+      )
+    ],
+    age:Annotated[
+    int,Path(
         ge=18,
         le=120,
-        description="Enter age")
-    ) -> dict:
-    return {"message": f"Информация о пользователе."
-                       f" Имя: {username}, Возраст: {age}"}
+        description='Укажите возраст'
+                    '(положительное целое число от 18 до 120)'
+      )
+    ]
+) -> dict:
+    return {
+        "message":(
+        f"Информация о пользователе."
+        f" Имя: {username}, Возраст: {age}"
+      )
+    }
 
 
 
